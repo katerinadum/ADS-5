2b2ea5e419a3ddf18681f6d77785f69c6d02d398
katerinadum <katya05102002@gmail.com>
2021-05-14 20:22:02 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,10 +5,86 @@
 
 template<typename T>
 class TPQueue {
-  // Сюда помещается описание структуры "Очередь с приоритетами"
-};
+  struct ITEM {
+    T data;
+    ITEM * next;
+  };
+ public:
+  TPQueue() : head(nullptr), tail(nullptr) {}
+  ~TPQueue();
+  void push(const T&);
+  T pop;
+//void print() const; //idk if should i do this??
+ private:
+  TPQueue:: ITEM * create(const T&);
+  ITEM *head;
+  ITEM *tail;
 
-struct SYM {
+template <typename T>
+TPQueue<T>::~TPQueue()
+{
+  while (head)
+  pop();
+}
+
+template <typename T>
+typename TPQueue <T>::ITEM *TPQueue <T>::create(const T&)data
+{
+  ITEM *item = new ITEM;
+  item->data = data;
+  item->next = nullptr;
+  return item;
+}
+
+template <typename T>
+void TPQueue <T>::push(const T& data);
+{
+  if (head && tail) {
+    ITEM *temp = head;
+    if (temp->data.prior < data.prior)
+    {
+      temp = create(data);
+      temp->next = head;
+      head = temp;
+    } else {
+      while (temp->next)
+      {
+        if (temp->next->data.prior < data.prior)
+        {
+          ITEM *t = create(data);
+          t->next = temp->next;
+          temp->next = t;
+          break;
+        } else
+          temp = temp->next;
+      }
+    }
+    if (!temp->next)
+    {
+      tail->next = create(data);
+      tail = tail->next;
+    } else
+  {
+    head = create(data);
+    tail = head;
+  }
+}
+
+template <typename T>
+T TPQueue<T>::pop()
+{
+  if (head) {
+    ITEM* temp = head->next;
+    T data = head->data;
+    delete head;
+    head = temp;
+    return data;
+  } else
+    return (T)0;
+}
+
+struct SYM
+{
   char ch;
   int  prior;
 };
